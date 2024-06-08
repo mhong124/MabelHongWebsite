855abb6ea055e00769513fffb69064c627716580
mhong124 <mhong124@terpmail.umd.edu>
2024-06-07 22:11:41 -0400

    updated skills and contact icons

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -12,15 +12,15 @@ export const Contact = () => {
         </div>
         <ul className = {styles.links}>
                 <li className = {styles.link}>
-                    <img src = {getImageUrl("hero/email.png")} alt = "Email Icon" height = "50" width = "50"></img>
+                    <img className = {styles.float} src = {getImageUrl("hero/email.png")} alt = "Email Icon" height = "50" width = "50"></img>
                     <a href = "mailto:mabelg.hong@gmail.com" target="_blank">mabelg.hong@gmail.com</a>
                 </li>
                 <li className = {styles.link}>
-                    <img src = {getImageUrl("contact/linkedin.png")} alt = "Linked Icon" height = "50" width = "50"></img>
+                    <img className = {styles.float} src = {getImageUrl("contact/linkedin.png")} alt = "Linked Icon" height = "50" width = "50"></img>
                     <a href = "https://www.linkedin.com/in/mabel-hong/" target="_blank">linkedin.com/in/mabel-hong</a>
                 </li>
                 <li className = {styles.link}>
-                    <img src = {getImageUrl("contact/github.png")} alt = "Github Icon" height = "50" width = "50"></img>
+                    <img className = {styles.float} src = {getImageUrl("contact/github.png")} alt = "Github Icon" height = "50" width = "50"></img>
                     <a href = "https://github.com/mhong124" target="_blank">github.com/mhong124</a>
                 </li>
         </ul>
